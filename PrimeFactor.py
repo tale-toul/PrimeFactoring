56261330c92ad6dd5f8ea19735a06854b24fc3d3
Tale Toul <jose.jerez.ext@juntadeandalucia.es>
2016-03-30 13:32:26 +0200

    The function validate_factors is added. This function is used to make sure the results obtained when factoring the number are correct.  What it does is multiply the factors and check the result against the original number. The function is called at the end of the factorization process.

--- PrimeFactor.py
+++ PrimeFactor.py
@@ -4,6 +4,9 @@ It is simple but very inneficient, the factorization of a prime number
 like 45834473 takes more than 14 seconds
 '''
 #Test number 7102454841, takes about 1minute and 21 seconds
+
+#Version 1.0.1
+
 import sys
 
 #Number to factor
@@ -13,18 +16,41 @@ candidate=2
 #List of factors
 factors=[]
 
+#Parameters: num.- An integer 
+#            factors.- A list of integers
+#The function checks that multiplying the factors in the list factors renders the value
+#stored in the variable num
+def validate_factors(num,factors):
+    partial_result=1
+    for items in factors:
+        partial_result *= items
+    if partial_result == num:
+        return True
+    else:
+        return False
+
+
+
+#####MAIN#######
+
 if(len(sys.argv)>1):
     try:
         num=int(sys.argv[1])
     except:
         print "Could not convert",sys.argv[1], "into an integer"
-        exit(1)
+        print "Usage:",sys.argv[0], "<positive integer>"
+        exit(-1)
     while(candidate <= num):
         if(num%candidate == 0):
             factors.append(candidate)
             num /= candidate
         else:
             candidate +=1
-    print "Factors of",sys.argv[1],"=",factors
+    if validate_factors(int(sys.argv[1]),factors):
+        print "Factors of",sys.argv[1],"=",factors
+        exit(0)
+    else:
+        print "The result is wrong, multiplying",factors,"doesn't yield",sys.argv[1]
+        exit(-2)
 else:
     print "Usage:",sys.argv[0], "<positive integer>"
