3ff64fc3fb5188883dc9072ee347931bff202a7c
Tale Toul <jose.jerez.ext@juntadeandalucia.es>
2016-03-31 16:30:14 +0200

    Added an command line argument (--addtest) and a function "read_test_cases(file)" to read test cases, which are numbers with their factors, from a file. In the program the test cases are stored in a dictionary; when writing the test cases to a file, json is used to serialize the dictionary. Right now the serialization of the dictionary and saving the file to disk is done in the main program

--- PrimeFactor.py
+++ PrimeFactor.py
@@ -5,11 +5,13 @@ like 45834473 takes more than 14 seconds
 '''
 #Test number 7102454841, takes about 1minute and 21 seconds
 
-#Version 1.0.3
+#Version 1.0.4
 
 import sys
 import time
 import argparse
+import os.path
+import json
 
 #List of factors
 factors=[]
@@ -50,18 +52,49 @@ def parse_arguments():
     parser=argparse.ArgumentParser(description="Find the prime factors of an integer number")
     parser.add_argument("num", help="Integer number to factor", type=int)
     parser.add_argument("-v", "--verbose", help="Verbose output", action="store_true")
-    return(parser.parse_args())
+    parser.add_argument("--addtest",  help="Adds the results of factoring this number, as a test case, to the file specified")
+    return(parser.parse_args()) 
+
+#Parameters: The file to read the test cases from
+#Return: the dictionary containing the test cases
+#Tries to read a file containing the json serialized dictionary containing the test
+#cases.  If the file doesn't exists returns an empty dictionary
+def read_test_cases(file):
+    test_cases_dict=dict()
+    if os.path.isfile(file): #If file exists, load it
+        try:
+            f_in=open(file)
+        except:
+            print "Could not open file",file,"to read"
+            exit(-2)
+        test_serialized=f_in.read()
+        test_cases_dict=json.loads(test_serialized)
+        f_in.close()
+    return test_cases_dict #If file doesn't exist return an empty dictionary
+
+
+
 
 
 #####MAIN#######
 
 arguments=parse_arguments()
+if arguments.addtest: test_cases=read_test_cases(arguments.addtest) #Load or create a dictionary of test cases
 t_start=time.time()
 factors=factorize(arguments.num)
 t_end=time.time()
 if validate_factors(arguments.num,factors):
-    print "Factors of",sys.argv[1],"=",factors,
+    print "Factors of",arguments.num,"=",factors,
     if arguments.verbose: print "In",t_end-t_start,"seconds"
+    if arguments.addtest and arguments.num not in test_cases:#Save the test case if requested and it has not been saved before
+        test_cases[arguments.num]=factors
+        try:
+            f_out=open(arguments.addtest,"w")
+        except:
+            print "Could not open file",arguments.addtest,"to write"
+            exit(-3)
+        f_out.write(json.dumps(test_cases))
+        f_out.close()
     exit(0)
 else:
     print "The result is wrong, multiplying",factors,"doesn't yield",arguments.num
